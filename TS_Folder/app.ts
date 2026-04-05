be0d90d279034e060cf7dde1937381b2d66d6549
enverJ <Enlem1987@gmail.com>
2026-04-04 22:49:31 -0600

    ts

---
+++ TS_Folder/app.ts
@@ -0,0 +1,12 @@
+let mesg: string = 'enver';
+console.log(mesg); // enver
+
+let x: number = 100;
+console.log(x);
+
+function testing(browser: string){
+    console.log(browser.length);
+
+}
+
+testing("chrome");
